645f18003f61858760c8e5c4d052f1e463b5f0d6
Alexandr Yaroslav <47974131+SUNR1seLIVE@users.noreply.github.com>
2021-05-17 12:07:22 +0300

    17/05/21-1 Доделал main-table Сделал сортировку

--- src/js/selected_items_message.js
+++ src/js/selected_items_message.js
@@ -1,13 +1,20 @@
 {
+  //#region // ! Выделение строк в окне message + scrollTo (0,0) при открытии окна
   const items = document.getElementsByClassName('item');
+  const popupContents = document.getElementsByClassName('message');
+  // const content_items = document.getElementsByClassName('popup__content-items').getElementsByClassName('popup__content message');
 
+  // ! selected rows
   for (let item of items) {
     item.addEventListener('click', () => {
       item.classList.toggle("selected");
     })
   }
 
-  
+  // ! scrollTo(0, 0)
+  //#endregion
+
+
   //#region // ! Выделение в главной таблици
   const inputs = document.getElementById('main-table__items').getElementsByClassName('checkbox-input');
   const check_all_rows = document.getElementById('main-table-select-all');
@@ -19,7 +26,7 @@
       for (let input of inputs) {
         input.checked = true;
       }
-    }else{
+    } else {
       for (let input of inputs) {
         input.checked = false;
       }
@@ -29,27 +36,70 @@
   for (let input of inputs) {
     input.addEventListener('change', () => {
 
-      if (!input.checked){
-        if(check_all_rows.checked){
+      if (!input.checked) {
+        if (check_all_rows.checked) {
           check_all_rows.checked = false;
         }
-      }else{
+      } else {
         for (let input of inputs) {
-          if (input.checked){
+          if (input.checked) {
             all_selected = true;
           } else {
             all_selected = false;
             return;
           }
         }
-        if(!all_selected){
+        if (!all_selected) {
           check_all_rows.checked = false;
-        }else{
+        } else {
           check_all_rows.checked = true;
         }
       }
     })
   }
+  //#endregion
+  //#region // ! Сортировка в главной таблици
+  /**
+ * Сортировка таблицы HTML.
+ * 
+ * @param {HTMLTableElement} table_header Таблица заголовков
+ * @param {HTMLTableElement} table_items Таблица для сортировки
+ * @param {number} column Индекс столбца для сортировки
+ * @param {boolean} asc Определяет, будет ли сортировка по возрастанию
+ */
+  function sortTableByColumn(table_header, table_items, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table_items.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll('tr'));
+    //Сортируем каждую строку
+    const sortedRows = rows.sort((a, b) => {
+      const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+      const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+      return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+    // Удаляем все существующие строки из таблицы
+    while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+    };
+    // Повторно добавляем отсортированные строки
+    tBody.append(...sortedRows);
+    // Запомнить, как сейчас сортируется столбец
+    table_header.querySelectorAll('th').forEach(th => th.classList.remove('sort-up', 'sort-down'));
+    table_header.querySelector(`th:nth-child(${column + 1})`).classList.toggle('sort-up', asc);
+    table_header.querySelector(`th:nth-child(${column + 1})`).classList.toggle('sort-down', !asc);
+  }
+
+  document.querySelectorAll('#main-table__header th').forEach(cell => {
+    const cellIndex = Array.prototype.indexOf.call(cell.parentElement.children, cell);
+    if(cellIndex > 0){
+      cell.addEventListener('click', () => {
+        const table_header = document.getElementById('main-table__header')
+        const table_items = document.getElementById('main-table__items');
+        const currentIsAscending = cell.classList.contains('sort-up');
+        sortTableByColumn(table_header, table_items, cellIndex, !currentIsAscending);
+      });
+    }
+  });
   //#endregion
 
   //#region // ! ТОЛЬКО ДЛЯ ТЕСТОВ
